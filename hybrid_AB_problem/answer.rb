20a48f4e63095e42a1c38ac7ca51335767d9e7e3
ryosy383 <mosso.ryosy383@gmail.com>
2014-04-23 00:48:37 +0900

    first commit

---
+++ hybrid_AB_problem/answer.rb
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define A int main(){int a,b;scanf("%d%d",&a,&b);printf("%d",a+b);return 0;}
+
+#/*
+''''
+puts gets.to_i + gets.to_i
+=begin
+#'''
+print( int( input() ) + int( input() ) )
+''''
+=end
+#'''
+#*/
+A
+
